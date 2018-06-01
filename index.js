856e13a7492e6afcc4041f9c73aa9147f2b70eac
Valentin <6501764@gmail.com>
2018-06-01 03:34:56 +0300

    fdsfds

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const TeleBot = require('telebot');
 const bot = new TeleBot('600854833:AAEgjiOAwg8HAd9OJt-QUKB1OwEVacZiU-g');
 
 bot.on('text', (msg) => msg.reply.text(msg.text));
+bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
 bot.start();
